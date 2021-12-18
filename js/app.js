d34e0d9617c51bda38274118d29357b02ddbeba2
Willian-Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2021-12-18 00:04:18 -0500

    terminando la programacion en consola y empezando la programacion de las vistas

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-function isEqualS(str1,str2) {
-    return str1.toUpperCase() == str2.toUpperCase();
+function isEqualS(str1, str2) {
+  return str1.toUpperCase() == str2.toUpperCase();
 }
-function isEqual(str1,str2) {
-    return str1 == str2;
+function isEqual(str1, str2) {
+  return str1 == str2;
 }
 
 class Carta {
@@ -12,45 +12,60 @@ class Carta {
     this.color = color;
   }
 
-  presentarCarta(){
-      console.log(`${this.numero} de ${this.tipo} color:
-      ${this.color}`);
+  presentarCarta() {
+    console.log(`${this.numero} de ${this.tipo} color: ${this.color}`);
   }
 }
 const tiposCartas = [
-    ["Picas", "negro"],
-    ["Diamantes", "rojo"],
-    ["Corazones", "rojo"],
-    ["Rombos", "negro "],
-  ];
+  ["Picas", "negro"],
+  ["Diamantes", "rojo"],
+  ["Corazones", "rojo"],
+  ["Rombos", "negro "],
+];
 
 class Naipe {
   constructor() {
     this.naipe = [];
   }
 
-  generarNaipe() {
-    
-    tiposCartas.forEach(el => {
-        for (let i = 1; i <= 13; i++) {
-            this.naipe.push(new Carta(el[0],i,el[1]));
-        }
+  __numeroAleatorio() {
+    return Math.floor(Math.random() * (51 + 1));
+  }
+
+  barajarCartas(n=0){
+    this.naipe.forEach((el,index) => {
+      const uno = n==0?index:this.__numeroAleatorio();
+      const dos = this.__numeroAleatorio();
+      // console.log(uno,dos);
+      const aux1 = this.naipe[uno];
+      this.naipe[uno]=this.naipe[dos];
+      this.naipe[dos]=aux1;
     });
   }
 
-  presentarCartas(){
-    this.naipe.forEach(el => {
+  generarNaipe() {
+    tiposCartas.forEach((el) => {
+      for (let i = 1; i <= 13; i++) {
+        this.naipe.push(new Carta(el[0], i, el[1]));
+      }
+    });
+  }
+
+  presentarCartas() {
+    this.naipe.forEach((el) => {
       el.presentarCarta();
     });
   }
 }
 
 function ejecutable() {
-    // let cartaUno = new Carta();
-    const naipe = new Naipe();
-    naipe.generarNaipe();
-    naipe.presentarCartas();
-    // cartaUno.presentarCarta();
+  const naipe = new Naipe();
+  naipe.generarNaipe();
+  naipe.presentarCartas();
+  naipe.barajarCartas();
+  console.log("===================================== Cartas Barajadas =====================================");
+  naipe.presentarCartas();
+  console.log(`tamaño del arreglo: ${naipe.naipe.length}`);
 }
 
 ejecutable();
