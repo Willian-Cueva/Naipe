b0ef236d48db7c958117086c68c93352497e98af
Willian-Cueva <69866069+Willian-Cueva@users.noreply.github.com>
2022-01-11 15:13:50 -0500

    por pasos xd

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ function isEqual(str1, str2) {
   return str1 == str2;
 }
 
-function camiarImagen(id='1_1',src="/assets/images/caraOculta.png"){
-  document.getElementById(id).setAttribute('src',src);
+function camiarImagen(id = "1_1", src = "/assets/images/caraOculta.png") {
+  document.getElementById(id).setAttribute("src", src);
 }
 
 class Carta {
@@ -14,8 +14,8 @@ class Carta {
     this.tipo = tipo;
     this.numero = numero;
     this.color = color;
-    this.estado = false;
-    this.urlImg = '/assets/images/caraOculta.png';
+    this.estado = true;
+    this.urlImg = "/assets/images/caraOculta.png";
   }
 
   presentarCarta() {
@@ -44,14 +44,12 @@ class Naipe {
     this.naipe.forEach((el, index) => {
       const uno = n == 0 ? index : this.__numeroAleatorio();
       const dos = this.__numeroAleatorio();
-      // console.log(uno,dos);
       const aux1 = this.naipe[uno];
       this.naipe[uno] = this.naipe[dos];
       this.naipe[dos] = aux1;
     });
-
   }
-  
+
   generarNaipe() {
     tiposCartas.forEach((el) => {
       for (let i = 1; i <= 13; i++) {
@@ -59,34 +57,34 @@ class Naipe {
       }
     });
   }
-  
+
   presentarCartas() {
     this.naipe.forEach((el) => {
       el.presentarCarta();
     });
   }
-  
+
   intensidadBarajada(i = 1) {
     for (let j = 0; j < i; j++) {
       this.barajarCartas();
     }
   }
   cargarImagenesCartas() {
-    const urlI='/assets/images/';
+    const urlI = "/assets/images/";
     const tiposCartas = {
-      0:["Picas", "negro"],
-      1:["Diamantes", "rojo"],
-      2:["Corazones", "rojo"],
-      3:["Rombos", "negro"],
-  };
+      0: ["Picas", "negro"],
+      1: ["Diamantes", "rojo"],
+      2: ["Corazones", "rojo"],
+      3: ["Rombos", "negro"],
+    };
     let cont = 0;
-    this.naipe.forEach((el)=>{
+    this.naipe.forEach((el) => {
       el.urlImg = `${urlI}${el.numero}${tiposCartas[cont][0]}${tiposCartas[cont][1]}.png`;
-      if (++cont>3) {
-        cont=0;
+      if (++cont > 3) {
+        cont = 0;
       }
-    })
-  };
+    });
+  }
 }
 
 class Monton {
@@ -100,21 +98,19 @@ class Monton {
     });
   }
 
-  presentarCarta(m=1) {
-    this.cartas.forEach((el,index) => {
+  presentarCarta(m = 1) {
+    this.cartas.forEach((el, index) => {
       if (el.estado) {
-        camiarImagen(`${m}_${index+1}`,el.urlImg);
+        camiarImagen(`${m}_${index + 1}`, el.urlImg);
       }
     });
   }
 
   inserto(carta) {
     this.cartas.push(carta);
-    // this.destapo();
   }
 
   retiro() {
-    // this.destapo();
     const retorno = this.cartas[0];
     this.cartas.shift();
     return retorno;
@@ -125,10 +121,11 @@ class Monton {
     return this.cartas[0].numero;
   }
 
-  ordenadas(){
-    let numero = this.cartas[0].numero,bandera = true;
+  ordenadas() {
+    let numero = this.cartas[0].numero,
+      bandera = true;
     for (let i = 1; i < this.cartas.length; i++) {
-      if (this.cartas[i].numero!=numero) {
+      if (this.cartas[i].numero != numero) {
         bandera = false;
       }
     }
@@ -142,7 +139,17 @@ class Juego {
     this.mesa = [];
   }
 
-  resetear(){
+  traductor(){
+    this.mesa.forEach((monton,index)=>{
+      monton.cartas.forEach((carta,indexC)=>{
+        if (carta.estado) {
+          camiarImagen(`${index+1}_${indexC}`,carta.urlImg)
+        }
+      })
+    })
+  }
+
+  resetear() {
     this.iniciarMesa();
   }
 
@@ -162,68 +169,64 @@ class Juego {
       const monton = new Monton();
       for (let j = 0; j < 4; j++) {
         monton.cartas.push(this.naipe.naipe[cont++]);
-        camiarImagen(`${i+1}_${j+1}`);
+        camiarImagen(`${i + 1}_${j}`);
       }
       this.mesa.push(monton);
     }
   }
-  presentarCarta(){
-    this.mesa.forEach((el,index)=>{
-      el.presentarCarta(index+1);
-    })
+  presentarCarta() {
+    this.mesa.forEach((el, index) => {
+      el.presentarCarta(index + 1);
+    });
   }
 
   jugar() {
-    
-    let ant = 13,numero=0,iteraciones=0;
+    let ant = 13,
+      numero = 0,
+      iteraciones = 0;
     while (!this.mesa[ant - 1].cartas[0].estado) {
+      // alert("Siguiente")
       numero = this.mesa[ant - 1].destapo();
-      this.presentarCarta();
+      // this.presentarCarta();
       this.mesa[numero - 1].inserto(this.mesa[ant - 1].retiro());
-      ant=numero;
+      ant = numero;
       iteraciones++;
+      // this.traductor();
     }
-    console.log('el juego terminó en '+iteraciones+" iteraciones -------------------------------------------------------------");
-    alert('el juego terminó en '+iteraciones+" iteraciones ");
-    
+    console.log(
+      "el juego terminó en " +
+        iteraciones +
+        " iteraciones -------------------------------------------------------------"
+    );
+    // alert("el juego terminó en " + iteraciones + " iteraciones ");
   }
 
-  comprobar(){
+  comprobar() {
     let bandera = true;
-    this.mesa.forEach(el=>{if (!el.ordenadas()) {
-      console.log('si entra xd');
-      bandera = false;
-    }})
+    this.mesa.forEach((el) => {
+      if (!el.ordenadas()) {
+        console.log("si entra xd");
+        bandera = false;
+      }
+    });
     return bandera;
   }
-
 }
 
 function ejecutable() {
-  // const naipe = new Naipe();
-  // naipe.generarNaipe();
-  // naipe.presentarCartas();
-  // // naipe.barajarCartas();
-  // naipe.intensidadBarajada(7);
-  // console.log("===================================== Cartas Barajadas =====================================");
-  // naipe.presentarCartas();
-  // console.log(`tamaño del arreglo: ${naipe.naipe.length}`);
   const juego = new Juego();
   juego.iniciarMesa();
   juego.presentarMontones();
-  // juego.mesa[0].destapo();
-  juego.jugar();
-  // juego.mesa[0].inserto(juego.mesa[1].retiro());
-  juego.presentarMontones();
+  // juego.jugar();
+  // juego.presentarMontones();
+  // juego.traductor();
 
   if (juego.comprobar()) {
-    console.log('La baraja fué ordenada en su totalidad');
+    console.log("La baraja fué ordenada en su totalidad");
   } else {
-    console.log('No se ordenó la baraja');
-    
+    console.log("No se ordenó la baraja");
   }
-  // camiarImagen();
-
+  juego.traductor();
 }
 
 ejecutable();
